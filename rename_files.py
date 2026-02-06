eb384f513b6d2471ba971700f7d709aeacb0c3bf
AnkurDe <ankurde2005@gmail.com>
2026-02-07 00:02:51 +0530

    Initial Commit

---
+++ rename_files.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import re
+
+FOLDER = Path("Data")
+reg = re.compile(r'\.([^.]+)$')
+
+for folder in FOLDER.iterdir():
+    counter = 1
+    if folder.is_dir():
+        print(folder.name)
+        for file in sorted(folder.iterdir()):
+            if file.is_file():
+                ext = reg.search(file.name)
+                new_name = file.with_name(str(counter) + "." + ext.group(1))
+                file.rename(new_name)
+                counter += 1
+
